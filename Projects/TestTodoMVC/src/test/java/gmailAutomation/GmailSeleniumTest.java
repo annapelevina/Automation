47cfb70bc982eb30cf9737fec3cedf4c1580776b
anna.pelevina <anna.pelevina@engagepoint.com>
2015-07-20 21:26:29 +0300

    Added Gmail Selemiub basic steps-Selenium_Intro_HomeWork

--- Projects/TestTodoMVC/src/test/java/gmailAutomation/GmailSeleniumTest.java
+++ Projects/TestTodoMVC/src/test/java/gmailAutomation/GmailSeleniumTest.java
@@ -4,12 +4,15 @@ import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 import static gmailAutomation.credentials.Credentials.login;
 import static gmailAutomation.credentials.Credentials.password;
+import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 /**
@@ -18,33 +21,36 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElem
 public class GmailSeleniumTest {
 
     WebDriver driver = new FirefoxDriver();
-    WebDriverWait wait = new  WebDriverWait (driver, 5);
+    WebDriverWait wait = new  WebDriverWait (driver, 10);
 
     @Test
     public void testGmailLoginSendSearchSelenium() {
 //login
         driver.get("https://mail.google.com/");
+        wait.until(visibilityOfElementLocated(By.id("Email")));
         driver.findElement(By.id("Email")).sendKeys(login);
         driver.findElement(By.id("next")).click();
+        wait.until(visibilityOfElementLocated(By.id("Passwd")));
         driver.findElement(By.id("Passwd")).sendKeys(password);
         driver.findElement(By.id("signIn")).click();
 
 //create email
         wait.until(visibilityOfElementLocated(By.xpath("//div[contains(text(), 'COMPOSE')]"))).click();
-        wait.until(visibilityOfElementLocated(By.linkText("COMPOSE"))).click();
+        wait.until(visibilityOfElementLocated(By.name("subjectbox")));
         driver.findElement(By.name("to")).sendKeys("selenidetest1@gmail.com");
         driver.findElement(By.name("subjectbox")).sendKeys("Test Gmail Subject");
-        driver.findElement(By.xpath("//div[contains(text(), 'COMPOSE')]")).click();
+        driver.findElement(By.xpath("//div[contains(text(), 'Send')]")).click();
 
 //refresh mailbox
-        driver.findElement((By.cssSelector("a[href='#inbox']"))).click();
+        driver.findElement((By.partialLinkText("Inbox"))).click();
 
 //verify delivered email in the top
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(".y6"), "Test Gmail Subject"));
-       // $$(".y6 :nth-child(1) b").get(0).shouldHave(text("Test Gmail Subject"));
+        List<WebElement> elements = driver.findElements(By.cssSelector("[role=\"main\"] .xT span"));
+        //or driver.findElements(By.xpath("//span//.[contains(text(), 'Test Gmail Subject')]")
+        assertEquals(elements.get(0).getText(), "Test Gmail Subject");
 
-//verify delivered email is only one unread
-        //$$(".y6 :nth-child(1) b").filter(text("Test Gmail Subject")).shouldHaveSize(1);
+//verify email is only one unread
+        assertEquals(driver.findElements(By.xpath("//span//.[contains(text(), 'Test Gmail Subject')]")).size(),1);
 
     }
 
---
+++ Projects/classWork16july/src/test/java/com/anna/MyCondition.java
@@ -0,0 +1,10 @@
+package com.anna;
+
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+/**
+ * Created by anna.pelevina on 7/16/2015.
+ */
+public class MyCondition implements ExpectedCondition<Boolean> {
+
+}
